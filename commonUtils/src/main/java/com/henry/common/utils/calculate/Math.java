f3f3551917dc0d7baeedc4022d80c33a43e79fca
莫闲 <774939983@qq.com>
2021-06-28 19:23:21 +0800

    1、增加数学类的计算

---
+++ commonUtils/src/main/java/com/henry/common/utils/calculate/Math.java
@@ -0,0 +1,30 @@
+package com.henry.common.utils.calculate;
+
+/**
+ * =============================================
+ * 时间：2021-06-28 19:21 </br>
+ * 作者：码牛教育[Henry老师]</br>
+ * 描述：数学相关的计算类
+ * ============================================
+ */
+public class Math {
+
+
+
+    /**
+     * 数学中次幂计算
+     *
+     * @param number 被计算的数据
+     * @param times  次幂的大小
+     * @return
+     */
+    public static int power(int number, int times) {
+        if (times < 1) {
+            return 0;
+        } else if (times == 1) {
+            return number;
+        } else {
+            return number * power(number, times - 1);
+        }
+    }
+}
